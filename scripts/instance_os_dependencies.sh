8fcdcc615d9bc18e2a72f6299c7d4da23b9345e1
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2024-09-02 20:40:59 +0530

    Update instance_os_dependencies.sh

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-sudo killall apt apt-get dpkg
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/lib/apt/lists/lock
-sudo dpkg --configure -a
+#sudo killall apt apt-get dpkg
+#sudo rm /var/lib/dpkg/lock-frontend
+#sudo rm /var/lib/apt/lists/lock
+#sudo dpkg --configure -a
 sudo apt-get update
 
 sudo apt install -y python3-pip
