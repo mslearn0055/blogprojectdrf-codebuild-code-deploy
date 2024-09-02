07f1ac15480c78526267921bbc5c8ddecd0a704d
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2024-09-02 20:55:28 +0530

    Update python_dependencies.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-sudo chown -R ubuntu:ubuntu ~/blogprojectdrf-codebuild-code-deploy
-virtualenv /home/ubuntu/blogprojectdrf-codebuild-code-deploy/venv
-source /home/ubuntu/blogprojectdrf-codebuild-code-deploy/venv/bin/activate
-pip install -r /home/ubuntu/blogprojectdrf-codebuild-code-deploy/requirements.txt
+#sudo chown -R ubuntu:ubuntu ~/blogprojectdrf-codebuild-code-deploy
+#virtualenv /home/ubuntu/blogprojectdrf-codebuild-code-deploy/venv
+#source /home/ubuntu/blogprojectdrf-codebuild-code-deploy/venv/bin/activate
+#pip install -r /home/ubuntu/blogprojectdrf-codebuild-code-deploy/requirements.txt
+echo "hi"
