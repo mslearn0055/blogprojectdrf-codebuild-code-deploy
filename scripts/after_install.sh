e015f524b43980ee6855114c4d5a500d9025ad7f
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2024-09-02 20:23:46 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/bash
 sudo killall apt apt-get dpkg
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/lib/apt/lists/lock
-sudo dpkg --configure -a
 sudo apt-get update
+sudo apt-get update --fix-missing
 
 
 echo "Pull Finished"
