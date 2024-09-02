8b5cea10acfb5554399de75c1b661c5d92887843
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2024-09-02 20:33:48 +0530

    Update instance_os_dependencies.sh

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+sudo killall apt apt-get dpkg
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/lib/apt/lists/lock
+sudo dpkg --configure -a
+sudo apt-get update
 
-#sudo apt install -y python3-pip
-#sudo apt install -y nginx
-#sudo apt install -y virtualenv
+sudo apt install -y python3-pip
+sudo apt install -y nginx
+sudo apt install -y virtualenv
