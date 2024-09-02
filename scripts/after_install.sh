8a97306dcb1157385feae782fe805735e3960f98
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2024-09-02 20:18:40 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
+sudo killall apt apt-get dpkg
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/lib/apt/lists/lock
+sudo dpkg --configure -a
+sudo apt-get update
+
 
 echo "Pull Finished"
 sudo systemctl daemon-reload
