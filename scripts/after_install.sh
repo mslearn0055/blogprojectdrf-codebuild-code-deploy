aa1f618af973f4e74e120ac3aadd4a32badb1f79
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2024-09-02 20:27:11 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-sudo killall apt apt-get dpkg
-sudo apt-get update
-sudo apt-get update --fix-missing
+#sudo killall apt apt-get dpkg
+#sudo apt-get update
+#sudo apt-get update --fix-missing
 
 
 echo "Pull Finished"
-sudo systemctl daemon-reload
+#sudo systemctl daemon-reload
 sudo systemctl restart nginx
